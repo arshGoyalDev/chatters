a95d732e804355a812e15766323d1289db0a163a
arshGoyalDev <arshgoyalDev@gmail.com>
2024-10-08 18:33:06 +0530

    initialized server

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+const databaseURL = process.env.DATABASE_URL;
+
+app.use(
+  cors({
+    origin: [process.env.ORIGIN],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+const server = app.listen(app, () => {
+  console.log(`Server is running at https://localhost:${port}`);
+});
+
+mongoose
+  .connect(databaseURL)
+  .then(() => console.log(`DB connection successful`))
+  .catch((error) => console.log(error.message));
