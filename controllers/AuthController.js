49591194a0cbf09a3bfdc103c83472a826feb0cf
arshGoyalDev <arshgoyalDev@gmail.com>
2024-10-09 21:20:55 +0530

    add: login function

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import User from "../models/UserModel.js";
+import { compare } from "bcrypt";
 
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 
@@ -27,6 +28,47 @@ const signup = async (request, response, next) => {
       user: {
         id: user.id,
         email: user.email,
+        profileSetup: user.profileSetup,
+      },
+    });
+  } catch (error) {
+    console.log({ error });
+    return response.status(500).send("Internal Server Error");
+  }
+};
+
+const login = async (request, response, next) => {
+  try {
+    const { email, password } = request.body;
+
+    if (email && !password) {
+      return response.status(400).send("Email and Password is required");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return response.status(404).send("User with the given email not found");
+    }
+
+    const auth = await compare(password, user.password);
+
+    if (!auth) {
+      return response.status(400).send("Password is incorrect");
+    }
+
+
+
+    response.cookie("jwt", createToken(email, user.id), {
+      maxAge,
+      secure: true,
+      sameSite: "None",
+    });
+
+    return response.status(200).json({
+      user: {
+        id: user.id,
+        email: user.email,
         firstName: user.firstName,
         lastName: user.lastName,
         profilePic: user.profilePic,
@@ -38,7 +80,5 @@ const signup = async (request, response, next) => {
     return response.status(500).send("Internal Server Error");
   }
 };
-
-
 
 export { signup, login };
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { signup } from "../controllers/AuthController.js";
+import { login, signup } from "../controllers/AuthController.js";
 
 const authRoutes = Router();
 
 authRoutes.post("/sign-up", signup);
+authRoutes.post("/login", login);
 
 export default authRoutes;
