878907c92e4fda3700c9aa5564cda6db35ea7405
arshGoyalDev <arshgoyalDev@gmail.com>
2024-10-19 13:03:43 +0530

    add: types folder to handle ts types

---
+++ utils/types/authTypes.ts
@@ -0,0 +1,10 @@
+interface UserInfo {
+  email: string;
+  password: string;
+  profileSetup: boolean;
+  firstName: string;
+  lastName: string;
+  profilePic: string;
+}
+
+export type {UserInfo};
