9a3e47b193944ac2b1323d41994654a7d656f8da
arshGoyalDev <arshgoyalDev@gmail.com>
2024-10-20 17:05:16 +0530

    add: getUserInfo function

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,19 +4,21 @@ import { compare } from "bcrypt";
 
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 
-const createToken = (email, password) => {
-  return jwt.sign({ email, password }, process.env.JWT_KEY, { expiresIn: maxAge });
+const createToken = (email, userId) => {
+  return jwt.sign({ email, userId }, process.env.JWT_KEY, {
+    expiresIn: maxAge,
+  });
 };
 
 const signup = async (request, response, next) => {
   try {
-    const { email, password } = request.body;
+    const { firstName, lastName, email, password } = request.body;
 
     if (email && !password) {
       return response.status(400).send("Email and Password is required");
     }
 
-    const user = await User.create({ email, password });
+    const user = await User.create({ firstName, lastName, email, password });
 
     response.cookie("jwt", createToken(email, user.id), {
       maxAge,
@@ -28,8 +30,8 @@ const signup = async (request, response, next) => {
       user: {
         id: user.id,
         email: user.email,
-        firstName: "",
-        lastName: "",
+        firstName: user.firstName,
+        lastName: user.lastName,
         profilePic: "",
         profileSetup: user.profileSetup,
       },
@@ -60,8 +62,6 @@ const login = async (request, response, next) => {
       return response.status(400).send("Password is incorrect");
     }
 
-
-
     response.cookie("jwt", createToken(email, user.id), {
       maxAge,
       secure: true,
@@ -84,4 +84,28 @@ const login = async (request, response, next) => {
   }
 };
 
-export { signup, login };
+const getUserInfo = async (request, response, next) => {
+  try {
+    const userData = await User.findById(request.userId);
+
+    if (!userData) {
+      return response.status(404).send("User with the given id not found")
+    }
+
+    return response.status(200).json({
+      user: {
+        id: userData.id,
+        email: userData.email,
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        profilePic: userData.profilePic,
+        profileSetup: userData.profileSetup,
+      },
+    });
+  } catch (error) {
+    console.log({ error });
+    return response.status(500).send("Internal Server Error");
+  }
+};
+
+export { signup, login, getUserInfo };
