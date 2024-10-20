390fbe30d349194018abed419d9bda082127cd17
arshGoyalDev <arshgoyalDev@gmail.com>
2024-10-20 18:04:01 +0530

    add: getUserInfo func in lib

---
+++ lib/auth-functions.ts
@@ -0,0 +1,26 @@
+import { UserInfo } from "@/utils/types";
+
+import { apiClient } from "./api-client";
+import { GET_USER_INFO_ROUTE } from "@/utils/constants";
+
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+const getUserInfo = async (router: AppRouterInstance ,setUserInfo: (userInfo: UserInfo) => void) => {
+  try {
+    const response = await apiClient.get(GET_USER_INFO_ROUTE, {
+      withCredentials: true,
+    });
+
+    if (response.status === 200) {
+      setUserInfo(response.data.user);
+    } else {
+      throw new Error("No User Found");
+    }
+  } catch (error) {
+    if (error) {
+      router.push("/sign-up");
+    }
+  }
+};
+
+export {getUserInfo};
