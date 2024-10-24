0fef85c68f40056017e7b2844cfefee24dda719d
arshGoyalDev <arshgoyalDev@gmail.com>
2024-10-24 21:38:46 +0530

    add: profile pic remove feature

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -9,6 +9,7 @@ import { Input } from "@/components/inputs";
 import { apiClient } from "@/lib/api-client";
 import {
   ADD_PROFILE_PIC_ROUTE,
+  DELETE_PROFILE_PIC_ROUTE,
   HOST,
   UPDATE_PROFILE_ROUTE,
 } from "@/utils/constants";
@@ -74,7 +75,9 @@ const ProfilePage = () => {
   }, [userInfo]);
 
   const handleFileInputClick = () => {
-    fileUploadRef.current!.click();
+    if (!profilePic) {
+      fileUploadRef.current!.click();
+    }
   };
 
   const handleImageChange = async (
@@ -95,14 +98,6 @@ const ProfilePage = () => {
           setUserInfo(response.data.user);
           setProfilePic(response.data.user.profilePic);
         }
-
-        // const reader = new FileReader();
-        // reader.onload = () => {
-        //   setProfilePic(reader.result);
-        // }
-
-        // reader.readAsDataURL(file);
-        //
         console.log(response);
       } catch (error) {
         console.error(error);
@@ -110,23 +105,37 @@ const ProfilePage = () => {
     }
   };
 
-  // const handleDeleteImage = async () => {
+  const deleteProfilePic = async () => {
+    try {
+      const response = await apiClient.delete(DELETE_PROFILE_PIC_ROUTE, {
+        withCredentials: true,
+      });
 
-  // }
+      if (response.status === 200) {
+        const newUserInfo = userInfo;
+        newUserInfo.profilePic = "";
+
+        setProfilePic("");
+        setUserInfo(newUserInfo);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <main className="min-h-screen xl:p-8 grid place-content-center">
       <div className="flex flex-col lg:flex-row justify-center items-center gap-8">
         <div>
           <div className="relative bg-zinc-900 border-2 border-zinc-700 rounded-2xl overflow-hidden">
-            <button
+            <div
               onMouseEnter={() => setButtonHovered(true)}
               onMouseLeave={() => setButtonHovered(false)}
               onClick={handleFileInputClick}
               className="relative w-[80vw] h-[80vw] max-w-[360px] max-h-[360px] grid place-content-center"
             >
-              {buttonHovered && (
-                <div className="grid place-content-center absolute z-20 top-0 left-0 w-full h-full bg-zinc-950 bg-opacity-90 transition-all duration-300">
+              {buttonHovered && !profilePic && (
+                <div className="grid place-content-center absolute z-20 top-0 left-0 w-full h-full bg-zinc-950 bg-opacity-90 transition-all duration-300 cursor-pointer">
                   <span className="stroke-white">
                     <svg
                       width="100"
@@ -152,12 +161,45 @@ const ProfilePage = () => {
                 </div>
               )}
               {profilePic ? (
-                // eslint-disable-next-line @next/next/no-img-element
-                <img
-                  src={profilePic}
-                  alt={firstName}
-                  className="w-full h-full"
-                />
+                <div>
+                  {
+                    // eslint-disable-next-line @next/next/no-img-element
+                    <img
+                      src={profilePic}
+                      alt={firstName}
+                      className="w-full h-full"
+                    />
+                  }
+                  <button
+                    onClick={deleteProfilePic}
+                    className="absolute top-2 right-2 pt-1.5 p-2 bg-zinc-800 border-2 border-zinc-700 rounded-lg"
+                  >
+                    <span className="stroke-white">
+                      <svg
+                        width="24"
+                        height="30"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M19.97 10H3.96997V18C3.96997 21 4.96997 22 7.96997 22H15.97C18.97 22 19.97 21 19.97 18V10Z"
+                          strokeWidth="1.5"
+                          strokeMiterlimit="10"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                        <path
+                          d="M21.5 7V8C21.5 9.1 20.97 10 19.5 10H4.5C2.97 10 2.5 9.1 2.5 8V7C2.5 5.9 2.97 5 4.5 5H19.5C20.97 5 21.5 5.9 21.5 7Z"
+                          strokeWidth="1.5"
+                          strokeMiterlimit="10"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                    </span>
+                  </button>
+                </div>
               ) : (
                 <span className="fill-zinc-700">
                   <svg
@@ -181,7 +223,7 @@ const ProfilePage = () => {
                   </svg>
                 </span>
               )}
-            </button>
+            </div>
             <input
               type="file"
               ref={fileUploadRef}
