015b1bfcbbd2579e6d74d34238f50b123e3a33d7
arshGoyalDev <arshgoyalDev@gmail.com>
2024-10-25 16:27:31 +0530

    fix: issue on login page

--- lib/auth-functions.ts
+++ lib/auth-functions.ts
@@ -5,7 +5,7 @@ import { GET_USER_INFO_ROUTE } from "@/utils/constants";
 
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
-const getUserInfo = async (router: AppRouterInstance ,setUserInfo: (userInfo: UserInfo) => void) => {
+const getUserInfo = async (router: AppRouterInstance ,setUserInfo: (userInfo: UserInfo) => void, pathname: string) => {
   try {
     const response = await apiClient.get(GET_USER_INFO_ROUTE, {
       withCredentials: true,
@@ -18,7 +18,10 @@ const getUserInfo = async (router: AppRouterInstance ,setUserInfo: (userInfo: Us
     }
   } catch (error) {
     if (error) {
-      router.push("/sign-up");
+      if (pathname !== "/login") {
+        router.push("/sign-up");
+
+      }
     }
   }
 };
