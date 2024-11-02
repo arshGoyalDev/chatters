211701112f90f2b98ee5b8ee4616fe917870cbd1
arshGoyalDev <arshgoyalDev@gmail.com>
2024-11-02 21:14:33 +0530

    fix: formatted code

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -11,16 +11,20 @@ import {
 } from "../controllers/AuthController.js";
 
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
-// import multer = multer({dest: "up"})
+
 import multer from "multer";
 
 const authRoutes = Router();
+
 const upload = multer({ dest: "uploads/profile" });
 
 authRoutes.post("/sign-up", signup);
 authRoutes.post("/login", login);
+authRoutes.post("/logout", logout);
+
 authRoutes.get("/user-info", verifyToken, getUserInfo);
 authRoutes.post("/update-profile", verifyToken, updateProfile);
+
 authRoutes.post(
   "/add-profile-image",
   verifyToken,
@@ -28,6 +32,6 @@ authRoutes.post(
   addProfileImage
 );
 authRoutes.delete("/delete-profile-image", verifyToken, removeProfileImage);
-authRoutes.post("/logout", logout);
+
 
 export default authRoutes;
