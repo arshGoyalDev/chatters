ded9d7b610ab5bd9127dc158aabf8d0012bd7e4e
arshGoyalDev <arshgoyalDev@gmail.com>
2024-12-20 23:02:09 +0530

    add: preivew for video/unknown files

--- components/chat/Chat/components/FileDisplay.tsx
+++ components/chat/Chat/components/FileDisplay.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 
-
 import { useState, useEffect, useRef } from "react";
 
 import { HOST } from "@/utils/constants";
@@ -56,6 +55,57 @@ const FileDisplay = ({ filePath }: { filePath: string }) => {
 
   return (
     <>
+      {fileType === "file" && (
+        <div className="w-full flex items-center gap-4 bg-zinc-950 px-4 py-4 rounded-lg">
+          <div className="bg-zinc-900 p-3 rounded-lg">
+            <span className="stroke-white">
+              <svg
+                width="28"
+                height="28"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M22 10V15C22 20 20 22 15 22H9C4 22 2 20 2 15V9C2 4 4 2 9 2H14"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M22 10H18C15 10 14 9 14 6V2L22 10Z"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M7 13H13"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M7 17H11"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </span>
+          </div>
+          <p className="font-semibold">{filePath.split("/")[filePath.split("/").length - 1]}</p>
+        </div>
+      )}
+  
+      {fileType === "video" && (
+        <div className="bg-zinc-900 w-full rounded-lg">
+          <div>
+            <video src={`${HOST}/${filePath}`} controls className="rounded-lg" controlsList="nodownload"/>
+          </div>
+          <h2 className="font-semibold px-4 py-4">{filePath.split("/")[filePath.split("/").length - 1]}</h2>
+        </div>
+      )}
+
       {fileType === "image" && (
         <img
           src={`${HOST}/${filePath}`}
@@ -63,8 +113,9 @@ const FileDisplay = ({ filePath }: { filePath: string }) => {
           className="w-full h-full rounded-lg"
         />
       )}
+
       {fileType === "audio" && (
-        <div className="flex items-center justify-between w-[100%] px-4 py-4 bg-zinc-950 mx-auto rounded-lg">
+        <div className=" flex items-center justify-between w-full px-4 py-4 bg-zinc-950 mx-auto rounded-lg">
           <div className="flex items-center gap-3">
             <div className="p-3 bg-zinc-900 rounded-lg">
               <span className="stroke-white">
