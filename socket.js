aff52d4b229d9a62fdce5da78b28f8c20469453d
arshGoyalDev <arshgoyalDev@gmail.com>
2025-03-03 23:00:40 +0530

    fix: leaveMember && groupDelete functions

--- socket.js
+++ socket.js
@@ -2,8 +2,6 @@ import Message from "./models/MessagesModel.js";
 import Chat from "./models/ChatModel.js";
 import User from "./models/UserModel.js";
 
-// import { decryptMessage, encryptMessage } from "./cryptr/index.js";
-
 const setupSocket = (io) => {
   const userSocketMap = new Map();
 
@@ -66,106 +64,101 @@ const setupSocket = (io) => {
     }
   };
 
-  // const deleteGroup = async (groupId) => {
-  //   const group = await Group.findById(groupId).populate("groupAdmin");
+  const deleteGroup = async (chatId) => {
+    const chat = await Chat.findById(chatId).populate("chatAdmin");
 
-  //   if (group.messages && group.messages.length > 0) {
-  //     await Message.deleteMany({
-  //       _id: { $in: group.messages },
-  //     });
-  //   }
+    if (chat.messages & (chat.messages.length > 0)) {
+      await Message.deleteMany({
+        _id: { $in: chat.messages },
+      });
+    }
 
-  //   const finalData = {
-  //     groupId: group._id,
-  //     groupName: group.groupName,
-  //     groupAdmin: `${groupAdmin.firstName} ${groupAdmin.lastName}`,
-  //   };
+    const finalData = {
+      chatId: chat._id,
+      chatName: chat.chatName,
+      chatAdmin: {
+        _id: chat.chatAdmin._id,
+        name: `${chat.chatAdmin.firstName} ${chat.chatAdmin.lastName}`,
+      },
+    };
 
-  //   await Group.findByIdAndDelete(groupId);
+    await Chat.findByIdAndDelete(chatId);
 
-  //   if (group.groupMembers) {
-  //     group.groupMembers.forEach((memberId) => {
-  //       const memberSocketId = userSocketMap.get(memberId.toString());
-  //       if (memberSocketId) {
-  //         io.to(memberSocketId).emit("groupDeleted", finalData);
-  //       }
-  //     });
-  //   }
+    if (chat.chatMembers) {
+      chat.chatMembers.forEach((memberId) => {
+        const memberSocketId = userSocketMap.get(memberId.toString());
+        if (memberSocketId) {
+          io.to(memberSocketId).emit("groupDeleted", finalData);
+        }
+      });
+    }
 
-  //   const adminSocketId = userSocketMap.get(group.groupAdmin._id.toString());
-  //   if (adminSocketId) {
-  //     io.to(adminSocketId).emit("groupDeleted", finalData);
-  //   }
-  // };
+    const adminSocketId = userSocketMap.get(chat.chatAdmin._id.toString());
+    if (adminSocketId) {
+      io.to(adminSocketId).emit("groupDeleted", finalData);
+    }
+  };
 
-  // const leaveGroup = async (data) => {
-  //   const { groupId, leavingMember } = data;
+  const leaveGroup = async (data) => {
+    const { chatId, leavingMember } = data;
 
-  //   const group = await Group.findById(groupId).populate("groupMembers");
+    const chat = await Chat.findById(chatId).populate("chatMembers");
 
-  //   const groupMembers = group.groupMembers.filter((member) =>
-  //     member._id.toString() !== leavingMember._id
-  //   );
+    const chatMembers = chat.chatMembers.filter(
+      (member) => member._id.toString() !== leavingMember._id
+    );
 
-  //   const leavingMessage = await Message.create({
-  //     sender: leavingMember._id,
-  //     recipient: null,
-  //     content: encryptMessage(
-  //       `${leavingMember.firstName} ${leavingMember.lastName} left the group`
-  //     ),
-  //     messageType: "leaving",
-  //     timeStamp: new Date(),
-  //     fileUrl: null,
-  //   });
+    const leavingMessage = await Message.create({
+      sender: leavingMember._id,
+      recipient: chatId,
+      content: `${leavingMember.firstName} ${leavingMember.lastName} left the group`,
+      messageType: "leaving",
+      timeStamp: new Date(),
+      fileUrl: null,
+    });
 
-  //   const messageData = await Message.findById(leavingMessage._id)
-  //     .populate(
-  //       "sender",
-  //       "id email profilePic firstName lastName status userOnline"
-  //     )
-  //     .exec();
+    const messageData = await Message.findById(leavingMessage._id)
+      .populate("sender")
+      .exec();
 
-  //   await Group.findByIdAndUpdate(groupId, {
-  //     groupMembers: groupMembers,
-  //     $push: { messages: leavingMessage._id },
-  //   });
+    await Chat.findByIdAndUpdate(chatId, {
+      chatMembers,
+      $push: { messages: leavingMessage._id },
+    });
 
-  //   const updatedGroup = await Group.findById(groupId)
-  //     .populate("groupMembers")
-  //     .populate("groupAdmin");
+    const newChat = await Chat.findById(chatId)
+      .populate("chatMembers")
+      .populate("chatAdmin");
 
-  //   const finalData = {
-  //     messageData: {
-  //       ...messageData,
-  //       content: decryptMessage(messageData.content),
-  //     },
-  //     updatedGroup,
-  //     leavingMemberId: leavingMember._id,
-  //   };
+    const finalData = {
+      messageData,
+      newChat,
+      leavingMemberId: leavingMember._id,
+    };
 
-  //   if (groupMembers) {
-  //     updatedGroup.groupMembers.forEach((member) => {
-  //       const memberSocketId = userSocketMap.get(member._id.toString());
-  //       if (memberSocketId) {
-  //         io.to(memberSocketId).emit("memberLeft", finalData);
-  //       }
-  //     });
-  //   }
+    if (chatMembers) {
+      newChat.chatMembers.forEach((member) => {
+        const memberSocketId = userSocketMap.get(member._id.toString());
+        if (memberSocketId) {
+          io.to(memberSocketId).emit("memberLeft", finalData);
+        }
+      });
+    }
 
-  //   const adminSocketId = userSocketMap.get(
-  //     updatedGroup.groupAdmin._id.toString()
-  //   );
-  //   if (adminSocketId) {
-  //     io.to(adminSocketId).emit("memberLeft", finalData);
-  //   }
+    const adminSocketId = userSocketMap.get(newChat.chatAdmin._id.toString());
 
-  //   const leavingMemberSocketId = userSocketMap.get(
-  //     leavingMember._id.toString()
-  //   );
-  //   if (leavingMemberSocketId) {
-  //     io.to(leavingMemberSocketId).emit("memberLeft", finalData);
-  //   }
-  // };
+    if (adminSocketId) {
+      io.to(adminSocketId).emit("memberLeft", finalData);
+    }
+
+    const leavingMemberSocketId = userSocketMap.get(
+      leavingMember._id.toString()
+    );
+
+    if (leavingMemberSocketId) {
+      io.to(leavingMemberSocketId).emit("memberLeft", finalData);
+    }
+  };
 
   io.on("connection", async (socket) => {
     const userId = socket.handshake.query.userId;
@@ -184,8 +177,8 @@ const setupSocket = (io) => {
       console.log("No userId provided");
     }
 
-    // socket.on("leaveGroup", leaveGroup);
-    // socket.on("deleteGroup", deleteGroup);
+    socket.on("leaveGroup", leaveGroup);
+    socket.on("deleteGroup", deleteGroup);
     socket.on("sendMessage", sendMessage);
 
     socket.on("disconnect", () => disconnect(socket));
