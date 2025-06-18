286ea0b0630c91562324056af07e089ecea07324
arshGoyalDev <arshgoyalDev@gmail.com>
2025-06-19 00:24:50 +0530

    fix: next.config.mjs for docker deployments

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost"],
+    // Allow images from backend server
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8080',
+        pathname: '/uploads/**',
+      },
+    ],
+    // Nextjs Server can't access the backend on docker
+    // it work locally
+    // will fix it later
+    unoptimized: true,
   },
-};
+}
 
 export default nextConfig;
